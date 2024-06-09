6103dcab755ea89519f7a6585571f5c15a97c71f
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-06-09 15:30:06 +0500

    added more content on the homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,19 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import CarouslHome from './components/Carousel'
-import Image from 'next/image'
-import { Transition } from '@headlessui/react'
-import { Button } from '@/components/ui/button'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useEffect, useState } from 'react';
+ 
+import Image from 'next/image';
+import { Transition } from '@headlessui/react';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
-import Statisticshome from './components/Statisticshome'
-import ProgramsInitiativesHome from './components/ProgramsInitiativesHome'
-import PowerOfaMeal from './components/PowerOfaMealHome'
-import ImpactStorieshome from './components/ImpactStorieshome'
-import HowWeOperate from './components/HowWeOperate'
-import OurPartners from './components/OurPartners'
-import HowYoucanHelp from './components/HowYoucanHelp'
+import Statisticshome from './components/Statisticshome';
+import ProgramsInitiativesHome from './components/ProgramsInitiativesHome';
+import PowerOfaMeal from './components/PowerOfaMealHome';
+import ImpactStorieshome from './components/ImpactStorieshome';
+import HowWeOperate from './components/HowWeOperate';
+import OurPartners from './components/OurPartners';
+import HowYoucanHelp from './components/HowYoucanHelp';
+import CarouslHome from './components/Carousel';
 
 function Home() {
   const router = useRouter();
@@ -23,7 +24,6 @@ function Home() {
     setIsShow(true);
   }, []);
 
-  // Handle carousel loading completion
   const handleCarouselLoad = () => {
     setIsCarouselLoaded(true);
   };
@@ -41,32 +41,19 @@ function Home() {
           leaveFrom="translate-y-0 opacity-100"
           leaveTo="translate-y-full opacity-0"
         >
-          <div
-         
-          className='w-full h-full inset-0'>
-            <CarouslHome   onLoad={handleCarouselLoad} />
+          <div className='w-full h-full inset-0'>
+            <CarouslHome onLoad={handleCarouselLoad} />
           </div>
         </Transition>
       </div>
 
-      {/* Conditionally render the rest of the components only after the carousel has loaded */}
       {isCarouselLoaded && (
-        <>
-          {/* The power of a meal */}
+        <div className="relative">
           <PowerOfaMeal />
-
-          {/* Impact stories */}
           <ImpactStorieshome />
-
-          {/* How we operate */}
           <HowWeOperate />
-
-          {/* Our programs */}
           <ProgramsInitiativesHome />
-
-          {/* Donate Now */}
-          <div
-            className='flex py-12 flex-col w-full justify-center items-center space-y-4 md:py-20 p-4 rounded-lg'>
+          <div className='flex py-12 flex-col w-full justify-center items-center space-y-4 md:py-20 p-4 rounded-lg'>
             <h1 className='text-2xl md:text-4xl text-warmGreen font-bold'>
               A Meal Is Hope. Will You Help?
             </h1>
@@ -75,23 +62,17 @@ function Home() {
             </p>
             <Button
               onClick={() => {
-                router.push('/donate')
+                router.push('/donate');
               }}
               className='bg-warmRed'
             >
               Donate now
             </Button>
           </div>
-
-          {/* Statistics */}
           <Statisticshome />
-
-          {/* Our partners */}
           <OurPartners />
-
-          {/* How you can help */}
           <HowYoucanHelp />
-        </>
+        </div>
       )}
     </div>
   );
