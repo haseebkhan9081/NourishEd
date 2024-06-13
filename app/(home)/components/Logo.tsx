5fc2fb1785a8f9d47366760585db67e61bf9e55e
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-06-13 16:23:09 +0500

    changed back the logo

--- app/(home)/components/Logo.tsx
+++ app/(home)/components/Logo.tsx
@@ -3,18 +3,16 @@ import Link from 'next/link';
 
 function Logo() {
   return (
-    <Link href={'/'}>
-      <div className="w-2/5 md:w-1/5 lg:w-2/5 xl:w-1/5">
-        <Image
-          src={'/Logo.png'}
-          alt='logo'
-          width={150}
-          height={100}  
-          layout="responsive"  
-          className="mx-auto"
-        />
-      </div>
-    </Link>
+    <Link
+    href={'/'}
+    >
+    <Image
+    src={'/Logo.png'}
+    alt='logo'
+    width={150}
+   height={1}
+    />
+ </Link>
   );
 }
 
