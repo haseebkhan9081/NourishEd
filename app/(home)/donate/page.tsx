8635b177cee59ef0c16feaada4787d2a8fa1038b
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-06-18 00:13:15 +0500

    fix

--- app/(home)/donate/page.tsx
+++ app/(home)/donate/page.tsx
@@ -27,7 +27,7 @@ text-2xl
 <div
 className='grid
 grid-cols-1
-lg:grid-cols-2
+ 
 gap-36
 '>
 <div
@@ -38,25 +38,7 @@ flex
 items-center'>
   <MainForm />
 </div>
-<div
-className='w-[400px]
-flex-col
-p-4
-flex
-  p-4
-space-y-4
-'>
-   <Image
-   src={"/image1.png"}
-   className='rounded-sm'
-   alt=''
-   layout='responsive'
-   width={100}
-   height={100}
-   />
-   <div
-   className='text-slate-700'>Your generosity makes a world of difference. By selecting an amount to donate, youre directly contributing to NourishEds mission of eradicating hunger from classrooms in Pakistan. Every dollar you give ensures that students receive nutritious meals, allowing them to focus on their education and reach their full potential. Together, we can transform lives and create a brighter future for these children. Choose an amount below to begin your contribution and join us in making hunger-free classrooms a reality. Thank you for your support!</div>
-</div>
+ 
 </div>
 
      </div>
